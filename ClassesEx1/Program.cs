7cb8feb348970e40cf2749e1fa5ce1e4e3802772
Felipemart123 <felipemart1876@gmail.com>
2022-07-13 21:29:22 -0400

    no chnages

--- ClassesEx1/Program.cs
+++ ClassesEx1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
 
+namespace ClassesEx1
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "KIA";
+            myCar.Model = "Optima";
+            myCar.Year = 2019;
+
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Explorer",
+                Year = 2001
+            };
+        }
+           
+    }
+
+
+
+}
